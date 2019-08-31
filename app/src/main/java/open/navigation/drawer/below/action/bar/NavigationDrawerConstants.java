945786dfd65e407fc950f1d9727168fc6d8ab601
rohitnotes <rohitnotes24@gmail.com>
2019-08-31 13:38:00 +0530

    Initial commit

---
+++ app/src/main/java/open/navigation/drawer/below/action/bar/NavigationDrawerConstants.java
@@ -0,0 +1,8 @@
+package open.navigation.drawer.below.action.bar;
+
+public class NavigationDrawerConstants {
+    public final static String SHARE_TITLE = "Android Development Tutorials";
+    public final static String SHARE_MESSAGE = "Hey Friend";
+    public final static String SHARE_VIA = "Share Via";
+    public final static String SHARE_TEXT_TYPE = "text/plain";
+}
